b859830a49c070ff366cb628eab7649715d3db11
osian <oisa.nikifor@gmail.com>
2023-04-27 02:31:55 +0300

    Seminar3

---
+++ src/GB_HW/Seminar3/Task1.java
@@ -0,0 +1,50 @@
+package GB_HW.Seminar3;
+// Реализовать алгоритм сортировки слиянием
+public class Task1 {
+
+    public void mergeSort(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        if (arr.length > 1) {
+            int mid = arr.length / 2;
+            int[] left = new int[mid];
+            for (int i = 0; i < mid; i++) {
+                left[i] = arr[i];
+            }
+            int[] right = new int[arr.length - mid];
+            for (int i = mid; i < arr.length; i++) {
+                right[i - mid] = arr[i];
+            }
+            mergeSort(left);
+            mergeSort(right);
+            merge(left, right, arr);
+        }
+    }
+
+    private void merge(int[] left, int[] right, int[] arr) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            arr[k++] = left[i++];
+        }
+        while (j < right.length) {
+            arr[k++] = right[j++];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 10, 7, 3, 1, 9, 7, 4, 5, 8, 2 };
+        Task1 mergeSort = new Task1();
+        mergeSort.mergeSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
---
+++ src/GB_HW/Seminar3/Task3.java
@@ -0,0 +1,28 @@
+package GB_HW.Seminar3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+// Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее из этого списка.
+public class Task3 {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(10);
+        numbers.add(5);
+        numbers.add(18);
+        numbers.add(3);
+        numbers.add(7);
+
+        int max = Collections.max(numbers);
+        int min = Collections.min(numbers);
+        int sum = 0;
+        for (Integer number : numbers) {
+            sum += number;
+        }
+        double avg = (double) sum / numbers.size();
+
+        System.out.println("Max: "  + max);
+        System.out.println("Min: "  + min);
+        System.out.println("Avg: "  + avg);
+    }
+}
